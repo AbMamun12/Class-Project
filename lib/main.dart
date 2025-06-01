dd9a86d03ce76e537f44e74aea6a7d9d1e878980
Abdullah <abdullah.mamun8191@gmail.com>
2025-06-01 09:55:14 +0600

    All Basic Tools

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// class 8.2 List view, Grid view, Size box, Container
+// class 8.2 List view, Grid view, Size box, Container,Divider
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,17 +19,184 @@ class IntroApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<String> FriendList = [
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+    'abdullah',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.greenAccent,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Home'),
         backgroundColor: Colors.grey,
       ),
+      /*     body: ListView(
+        scrollDirection: Axis.vertical,
+          children: [
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Center(
+                    child: Text('Box 1'),
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Center(
+                    child: Text('Box 1'),
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Center(
+                    child: Text('Box 1'),
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Center(
+                    child: Text('Box 1'),
+                  ),
+                ),
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: Text('Box 1'),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: Text('Box 1'),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: Text('Box 1'),
+              ),
+            ),              SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: Text('Box 1'),
+              ),
+            ),              SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: Text('Box 1'),
+              ),
+            ),              SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: Text('Box 1'),
+              ),
+            ),              SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: Text('Box 1'),
+              ),
+            ),              SizedBox(
+              height: 100,
+              width: 100,
+              child: Center(
+                child: Text('Box 1'),
+              ),
+            ),
+
+
+
+
+
+          ],
+
+      ), */
+
+      /*   body: ListView.builder(
+        itemCount: 1000, // 0-999
+        itemBuilder: (contex, index){
+          return SizedBox(
+            width: 100,
+            height: 100,
+            child: Center(child: Text(index.toString())),
+          );
+
+        },
+      ), */
+      /*  body: ListView.builder(
+        itemCount:FriendList.length,
+     //   scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index){
+          return Text(FriendList[index]);
+        },
+      ), */
+    /*  body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 4,
+            childAspectRatio: 1),
+        itemCount: FriendList.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Text(index.toString()),
+              Text(FriendList[index]),
+            ],
+          );
+        },
+      ), */
+
+   /*   body: ListView.separated(
+        itemCount: FriendList.length,
+        itemBuilder: (contex, index){
+          return Column(
+          children: [
+            Text(FriendList[index]),
+          ],
+          );
+        },
+      separatorBuilder: (context, index){
+          return Divider(
+            height: 20,
+            color: Colors.red,
+            thickness: 2,
+          );
+          return Text('This is $index separator');
+      },
+      ), */
+
     );
   }
 }
