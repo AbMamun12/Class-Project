b97393ed69b5e19dc9078da59ddc6a1e3b9060e4
Abdullah <abdullah.mamun8191@gmail.com>
2025-06-05 17:54:43 +0600

    All Basic Tools

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,35 +1,76 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: Text('Profile'),
-          leading: Icon(CupertinoIcons.back),
+    return MaterialApp(home: HomeActivity());
+  }
+}
 
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  MySnackBar(message, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(8),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.black,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(60))));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Greetting App'),
         ),
-        child: Center(
-
-          child:Column(
+        body: Center(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
-              Text('This is Cupertino Design System '),
-              SizedBox(height: 14,),
-              Icon(CupertinoIcons.airplane),
-              SizedBox(height: 14,),
-              CupertinoButton(child: Text('Click'), onPressed: (){}),
-              SizedBox(height: 14,),
-              CupertinoButton.filled(child: Text('Click'), onPressed: (){}),
-
+              Text(
+                'Hello, World!',
+                style: TextStyle(
+                  //color:Color(Colors.red as int),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red),
+              ),
+              Text(
+                'Welcome to Flutter !',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: Center(
+                  child: Image.network(
+                      'https://storage.googleapis.com/cms-storage-bucket/0dbfcc7a59cd1cf16282.png'),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  MySnackBar('Button Pressed!', context);
+                },
+                child: Text(
+                  'Press Me',
+                  style: TextStyle(fontSize: 16),
+                ),
+                style: buttonStyle,
+              ),
             ],
           ),
-
-
-              ),
-        );
+        ));
   }
 }
