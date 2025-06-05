00772269e3dcf17a72c27183399289bb089dd678
Abdullah <abdullah.mamun8191@gmail.com>
2025-06-06 00:08:58 +0600

    All Basic Tools

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,95 @@
 import 'package:flutter/material.dart';
-
-void main() {
+void main(){
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeActivity());
+    return MaterialApp(
+      home: Home(),
+    );
   }
 }
-
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key});
-
-  MySnackBar(message, context) {
-    return ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
-  }
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-        padding: EdgeInsets.all(8),
-        backgroundColor: Colors.green,
-        foregroundColor: Colors.black,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(60))));
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Greetting App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    print(mediaQuery.size.width);
+    print(mediaQuery.size.height);
+    print(mediaQuery.size.aspectRatio);
+    print(mediaQuery.size.flipped.height);
+    print(mediaQuery.devicePixelRatio);
+
+    if(mediaQuery.size.width<640){
+      print('This is a PHONE');
+    }
+    else if (mediaQuery.size.width>640 && mediaQuery.size.width<1008){
+      print('This is a Tablet');
+    } else {
+      print('Laptop/Desktop');
+    }
+
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.blue,
+
+      ),
+      body: OrientationBuilder(
+        builder: (context, Orientation orientation) {
+          print(orientation);
+          /* return Column(
             children: [
-              Text(
-                'Hello, World!',
-                style: TextStyle(
-                  //color:Color(Colors.red as int),
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red),
+              Wrap(
+                alignment: WrapAlignment.center,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                spacing: 16,
+                children: [
+                  Text('dddddddddddddddd'),
+                  Text('dddddddddddddddd'),
+                  Text('dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd'),
+                  Text('dddddddddddddddd'),
+                  Text('dddddddddddddddd'),
+
+
+
+
+                ],
               ),
-              Text(
-                'Welcome to Flutter !',
-                style: TextStyle(
-                  fontSize: 24,
-                ),
-              ),
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: Center(
-                  child: Image.network(
-                      'https://storage.googleapis.com/cms-storage-bucket/0dbfcc7a59cd1cf16282.png'),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  MySnackBar('Button Pressed!', context);
-                },
-                child: Text(
-                  'Press Me',
-                  style: TextStyle(fontSize: 16),
-                ),
-                style: buttonStyle,
-              ),
+              Text(orientation.name),
             ],
-          ),
-        ));
+          );*/
+          if (orientation == Orientation.portrait) {
+            return Column(
+              children: [
+                Wrap(
+                  alignment: WrapAlignment.center,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  spacing: 16,
+                  children: [
+                    Text('dddddddddddddddd'),
+                    Text('dddddddddddddddd'),
+                    Text(
+                        'dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd'),
+                    Text('dddddddddddddddd'),
+                    Text('dddddddddddddddd'),
+                  ],
+                ),
+                Text(orientation.name),
+              ],
+            );
+          } else {
+            return Center(
+              child: Text('Too Big Screen'),
+            );
+          }
+        }
+      ),
+    );
   }
-} 
+}
+
