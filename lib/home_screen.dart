cb5c9dab3a8af4c035a52b1e4ba515961f192327
Abdullah <abdullah.mamun8191@gmail.com>
2025-06-06 22:16:50 +0600

    Sum App, Simple Calculator

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,23 +5,42 @@ class HomeScreen extends StatefulWidget {
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
-
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _firstNumTEController = TextEditingController();
+  final TextEditingController _secondNumTEController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: const Text('Calculator'),
-
+        backgroundColor: Colors.green,
+        centerTitle: true,
       ),
-      body: Column(
-
-        children: [
-
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _firstNumTEController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: 'Firfdffst Number',
+                labelText: 'First Number',
+              ),
+            ),
+            TextField(
+              controller: _secondNumTEController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: 'Second Number',
+                labelText: 'Second Numyuber',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
