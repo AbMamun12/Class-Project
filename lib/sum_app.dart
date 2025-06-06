f36971670f944f506cad737f4789e0e5939d9c55
Abdullah <abdullah.mamun8191@gmail.com>
2025-06-06 16:57:44 +0600

    StateFull Widget

---
+++ lib/sum_app.dart
@@ -0,0 +1,14 @@
+import 'package:class_project/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class SumApp extends StatelessWidget {
+  const SumApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
