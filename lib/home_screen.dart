f5ca7c3a0aed9befef39c45aa3434cb874b40282
Abdullah <abdullah.mamun8191@gmail.com>
2025-06-10 11:46:14 +0600

    video module 13 class 1( water tracker app)

---
+++ lib/home_screen.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Waterdfgd TRa f  cker'),
+      ),
+      body: Column(
+        children: [
+          Text('Water Trackffffdddddddd dsfgf     yjrt          ddgggggger App')
+        ],
+      ),
+    );
+  }
+}
