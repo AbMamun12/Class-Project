1c6f2efd8844520a1362997fb8e6616f33ae2167
Abdullah <abdullah.mamun8191@gmail.com>
2025-06-11 14:46:36 +0600

    navigation(এক ইউজেট থেকে অন্য উইজেট )

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,71 @@
-import 'package:class_project/app.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-void main(){
-  runApp(WaterTrackerApp());
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.pink,
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Go to Profile'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SettingScreen extends StatefulWidget {
+  const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
